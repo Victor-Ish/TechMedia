74da6c4f02f12254946a735eba5c70720e004ec1
Victor Ish <victorish20@gmail.com>
2023-12-10 13:31:59 +0100

    techmedia updated

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
-//const copy = document.querySelector('.logos__slide').cloneNode(true)
-//document.querySelector('.logos').appendChild(copy)
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:12,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+alert('hello');
+
+const harmburger = document.querySelector('.harmburger')
+const navBar = document.querySelector('.nav__content')
+
+harmburger.addEventListener('click', () =>{
+    harmburger.classList.toggle('show')
+    navBar.classList.toggle('action')
+})
+const navLists = document.querySelectorAll('.nav__content li')
+navLists.forEach((item) =>{
+  item.addEventListener('click', function(){
+    harmburger.classList.remove('show')
+    navBar.classList.remove('action')
+  })
+})
